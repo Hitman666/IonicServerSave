246acf2321e9218f8d768d5fa6439a8af8dafd68
Hitman666 <nikola.breznjak@gmail.com>
2015-06-19 09:26:27 +0200

    finished project

---
+++ PHP/api.php
@@ -0,0 +1,38 @@
+<?php
+	//http://stackoverflow.com/questions/18382740/cors-not-working-php
+	if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
+
+    //http://stackoverflow.com/questions/15485354/angular-http-post-to-php-and-undefined
+    $postdata = file_get_contents("php://input");
+	if (isset($postdata)) {
+		$request = json_decode($postdata);
+		$username = $request->username;
+
+		if ($username != "") {
+			echo "Server returns: " . $username;
+		}
+		else {
+			echo "Empty username parameter!";
+		}
+	}
+	else {
+		echo "Not called properly with username parameter!";
+	}
+?>
